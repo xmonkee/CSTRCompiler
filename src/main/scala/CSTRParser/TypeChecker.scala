608d0f3b4ef013f77b15ec33d9341f847a7c3674
Mayank Mandava <mayankmandava@gmail.com>
2015-11-02 13:11:41 -0600

    More for constructs

--- src/main/scala/CSTRParser/TypeChecker.scala
+++ src/main/scala/CSTRParser/TypeChecker.scala
@@ -26,8 +26,11 @@ object TypeChecker {
     for {ParameterDeclaration(t, n) <- xs} yield LexTypeToType(t)
     }
 
-  case class SymbolTable (table: Map[Lexemes.Ident, Type], parent: Option[SymbolTable], fname: Option[Ident] = None)
-  {self =>
+  case class SymbolTable (
+                           table: Map[Lexemes.Ident, Type],
+                           parent: Option[SymbolTable],
+                           fname: Option[Ident] = None) {self =>
+
     def addSymbol(i: Ident, t: Type) = copy(table = table + (i -> t)) // Add to current Level
     def addLevel = SymbolTable(self, fname)
     def removeLevel = parent.get
@@ -35,8 +38,10 @@ object TypeChecker {
     def getCurrentLevel(i: Ident): Option[Type] = table.get(i) // get only from current Level
     def get(i: Ident): Option[Type] = // get recursively from current or parent
       table.get(i) match { case None => parent flatMap(_.get(i)) case x => x}
-    def containsCurrentLevel(i: Ident) = getCurrentLevel(i) match {case None => false case _ => true}
-    def contains(i: Ident) = get(i) match {case None => false case _ => true}
+    def containsCurrentLevel(i: Ident) =
+      getCurrentLevel(i) match {case None => false case _ => true}
+    def contains(i: Ident) =
+      get(i) match {case None => false case _ => true}
 
     def typeof(exp: Expression): Option[Type] = {
       def typeofexp(first: Expression, rest: Seq[(_, Expression)]): Option[Type] =
@@ -76,12 +81,18 @@ object TypeChecker {
   type State = Writer[List[String], SymbolTable]
   object State {
     def apply(w: List[String], a: SymbolTable): State = a.set(w)
-    def apply(a: SymbolTable): State = Writer(List.empty, a)
-    def apply(): State = Writer(List.empty, SymbolTable())
+    def apply(a: SymbolTable): State = State(List.empty, a)
+    def apply(): State = State(SymbolTable())
     def fold[A](seq: Seq[A], init: State, f: A => SymbolTable => State) =
-      seq.foldLeft(init){case (st, a) => st flatMap f(a)}
+      seq.foldLeft(init) {case (st, a) =>
+        st flatMap f(a)
+      }
     def foldExp(first: Expression, rest: Seq[(_, Expression)])(s: SymbolTable) =
-      fold(first +: (rest map (_._2)), State(s), ((e:Expression) => checkType(e, s.typeof(e), Integer)))
+      fold(
+        first +: (rest map (_._2)),
+        State(s),
+        ((e:Expression) => checkType(e, s.typeof(e), Integer))
+      )
   }
 
   def checkType[A](a: AST, received: Option[A], expected: A)(s: SymbolTable): State =
@@ -115,89 +126,98 @@ object TypeChecker {
     State(new_s)
   }
 
-    def typeCheck(a: Option[AST])(s: SymbolTable): State =
-      a match { case Some(a_) => typeCheck(a_)(s) case None => State(s)}
 
-    def typeCheck(a: AST)(s: SymbolTable): State = a match {
-      case Program(children: Seq[TopLevel]) =>
-        State.fold[AST](children, State(s), typeCheck)
+  def typeCheck(a: Option[AST])(s: SymbolTable): State =
+    a match { case Some(a_) => typeCheck(a_)(s) case None => State(s)}
 
-      case ExternDeclaration(t, decList: Seq[Declarator]) =>
-        State.fold[Declarator](decList, State(s), (addDeclarator(t, _)))
+  def typeCheck(a: AST)(s: SymbolTable): State = a match {
+    case Program(children: Seq[TopLevel]) =>
+      State.fold[AST](children, State(s), typeCheck)
 
-      case Declaration(t, decList) =>
-        State.fold[Declarator](decList, State(s), (addDeclarator(t, _)))
+    case ExternDeclaration(t, decList: Seq[Declarator]) =>
+      State.fold[Declarator](decList, State(s), (addDeclarator(t, _)))
 
-      case FunctionDefinition(retType, functionDeclarator, body) => {
-        val FunctionDeclarator(name, params) = functionDeclarator
-        val st1 =
-          if (s contains name) checkType(name, s.typeof(name), Func(params, retType))(s)  // check decl
-          else addDeclarator(retType, functionDeclarator)(s) // Just add new Declaration
-        for {
-          s1 <- st1
-          s2 <- addParams(params)(s1.addLevel.addFName(name))
-          s3 <- typeCheck(body)(s2)
-        } yield s3.removeLevel
-      }
+    case Declaration(t, decList) =>
+      State.fold[Declarator](decList, State(s), (addDeclarator(t, _)))
 
-      case CompoundInstruction(decList, instrList) =>
-        State.fold[AST](decList ++ instrList, State(s), typeCheck)
-
-      case Assignment(i, e) => checkType(a, s.typeof(i), s.typeof(e))(s)
-
-      case Condition(left, _, right) => for {
-        s1 <- checkType(left, s.typeof(left), Integer)(s)
-        s2 <- checkType(right, s.typeof(right), Integer)(s1)
-      } yield s2
-
-      case SelectInstruction(cond, then, _else) => for {
-        s1 <- typeCheck(cond)(s)
-        s2 <- typeCheck(then)(s1.addLevel)
-        s3 <- typeCheck(_else)(s2.removeLevel.addLevel)
+    case FunctionDefinition(retType, functionDeclarator, body) => {
+      val FunctionDeclarator(name, params) = functionDeclarator
+      val st1 =
+        if (s contains name)
+          checkType(name, s.typeof(name), Func(params, retType))(s)  // check decl
+        else
+          addDeclarator(retType, functionDeclarator)(s) // Just add new Declaration
+      for {
+        s1 <- st1
+        s2 <- addParams(params)(s1.addLevel.addFName(name))
+        s3 <- typeCheck(body)(s2)
       } yield s3.removeLevel
+    }
 
-      case While(cond, instr ) => for {
-        s1 <- typeCheck(cond)(s)
-        s2 <- typeCheck(instr)(s1.addLevel)
-      } yield s2.removeLevel
+    case CompoundInstruction(decList, instrList) =>
+      State.fold[AST](decList ++ instrList, State(s), typeCheck)
 
-      case Do(cond, instr) => for {
-        s1 <- typeCheck(cond)(s)
-        s2 <- typeCheck(instr)(s1.addLevel)
-      } yield s2.removeLevel
+    case Assignment(i, e) => checkType(a, s.typeof(i), s.typeof(e))(s)
 
-      case For(init, cond, fin, instr) => for {
-        s1 <- typeCheck(init)(s)
-        s2 <- typeCheck(cond)(s1)
-        s3 <- typeCheck(fin)(s2)
-        s4 <- typeCheck(instr)(s3.addLevel)
-      } yield s4.removeLevel
+    case Condition(left, _, right) => for {
+      s1 <- checkType(left, s.typeof(left), Integer)(s)
+      s2 <- checkType(right, s.typeof(right), Integer)(s1)
+    } yield s2
 
-      case Return(exp) => for {
-        s1 <- checkType(a, s.fname flatMap s.typeof map (_.asInstanceOf[Func].ret), s.typeof(exp))(s)
+    case SelectInstruction(cond, then, _else) => for {
+      s1 <- typeCheck(cond)(s)
+      s2 <- typeCheck(then)(s1.addLevel)
+      s3 <- typeCheck(_else)(s2.removeLevel.addLevel)
+    } yield s3.removeLevel
+
+    case While(cond, instr ) => for {
+      s1 <- typeCheck(cond)(s)
+      s2 <- typeCheck(instr)(s1.addLevel)
+    } yield s2.removeLevel
+
+    case Do(cond, instr) => for {
+      s1 <- typeCheck(cond)(s)
+      s2 <- typeCheck(instr)(s1.addLevel)
+    } yield s2.removeLevel
+
+    case For(init, cond, fin, instr) => for {
+      s1 <- typeCheck(init)(s)
+      s2 <- typeCheck(cond)(s1)
+      s3 <- typeCheck(fin)(s2)
+      s4 <- typeCheck(instr)(s3.addLevel)
+    } yield s4.removeLevel
+
+    case Return(exp) => {
+      val retType = for {
+        r1 <- s.fname
+        Func(_, r2) <- s.typeof(r1)
+      } yield r2
+      for {
+        s1 <- checkType(a, s.typeof(exp), retType)(s)
         s2 <- typeCheck(exp)(s1)
       } yield s2
-
-      //For Expressions, we check the constituent parts to be Integer
-      case Shiftive(first, rest) => State.foldExp(first, rest)(s)
-      case Additive(first, rest) => State.foldExp(first, rest)(s)
-      case Multiplicative(first, rest) => State.foldExp(first, rest)(s)
-      case Minus(exp) => checkType(exp, s.typeof(exp), Integer)(s)
-
-      case FunctionApplication(name, args) => {
-        val first = checkType(name, s get name, s get name)(s) // Check presence of function name
-        val params = s get name map (_.asInstanceOf[Func].args)
-        params match {
-          case Some(ps) if (args.length == ps.length) => // Check number of params
-            (ps zip args).foldLeft(first) { // Check types of params
-                case (acc: State, (expected, received)) =>
-                  acc flatMap checkType(received, s.typeof(received), expected)
-              }
-          case Some(ps) => first flatMap (State(Errors.wrongNumArgs(a), _))
-          case None => first flatMap (State(Errors.notFound(name), _))
-        }
-      }
-
-      case _ => State(s)
     }
+
+    //For Expressions, we check the constituent parts to be Integer
+    case Shiftive(first, rest) => State.foldExp(first, rest)(s)
+    case Additive(first, rest) => State.foldExp(first, rest)(s)
+    case Multiplicative(first, rest) => State.foldExp(first, rest)(s)
+    case Minus(exp) => checkType(exp, s.typeof(exp), Integer)(s)
+
+    case FunctionApplication(name, args) => {
+      val first = checkType(name, s get name, s get name)(s) // Check presence of function name
+      val params = s get name map (_.asInstanceOf[Func].args)
+      params match {
+        case Some(ps) if (args.length == ps.length) => // Check number of params
+          (ps zip args).foldLeft(first) { // Check types of params
+            case (acc: State, (expected, received)) =>
+              acc flatMap checkType(received, s.typeof(received), expected)
+          }
+        case Some(ps) => first flatMap (State(Errors.wrongNumArgs(a), _))
+        case None => first flatMap (State(Errors.notFound(name), _))
+      }
+    }
+
+    case _ => State(s)
+  }
 }
